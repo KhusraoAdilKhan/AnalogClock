50b9d3ef07bd45e5037c6a86b507ffcfcf2d1e9a
Khan <kkhan@comscore.com>
2024-03-05 00:44:37 +0530

    stop wach added

--- script.js
+++ script.js
@@ -36,3 +36,9 @@ function displayTime(){
 }
 
 setInterval(displayTime, 1000);
+function gotoStopWatch(){
+    window.location.href = "C://KhusraoAdil//LEARN//Projects//AnalogClock//stopwatch.html"
+}
+function gotoAnalogClock(){
+    window.location.href = "C://KhusraoAdil//LEARN//Projects//AnalogClock//index.html"
+}
